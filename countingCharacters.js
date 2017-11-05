2826e04a6cc380f25944dd0c015e83827246ab5e
bjackson0050 <31907029+bjackson0050@users.noreply.github.com>
2017-11-05 11:04:13 -0600

    Update countingCharacters.js

--- countingCharacters.js
+++ countingCharacters.js
@@ -18,3 +18,19 @@ function countingCharacters2(stringToCount, characterToFind){
     console.log("Number of times the character appears: " + 
                   characterCount);
 }
+function countingCharacters3(str, search){    
+    var count = 0;
+    var numChars = search.length;
+    
+    var lastIndex = str.length - numChars;
+    for (var index = 0; index <= lastIndex; index++){
+       var current = str.substring(index, index + numChars);
+        if (current == search){            
+            count++;
+        }
+    }
+  
+  console.log("String to search in: " + str);
+  console.log("Characters to find: " + search);
+  console.log("Number of times the character appears: " + count);    
+}    
